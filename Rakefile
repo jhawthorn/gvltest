8269a33b8f40b325b1637d450d33ae522e0d339c
John Hawthorn <john@hawthorn.email>
2024-05-11 02:58:57 -0700

    Add test and sleep_without_gvl

--- Rakefile
+++ Rakefile
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 require "bundler/gem_tasks"
+require "rake/testtask"
+
+Rake::TestTask.new(:test) do |t|
+  t.libs << "test"
+  t.libs << "lib"
+  t.test_files = FileList["test/**/test_*.rb"]
+end
+
 require "rake/extensiontask"
 
 task build: :compile
@@ -11,4 +19,4 @@ Rake::ExtensionTask.new("gvltest", GEMSPEC) do |ext|
   ext.lib_dir = "lib/gvltest"
 end
 
-task default: %i[clobber compile]
+task default: %i[clobber compile test]
--- ext/gvltest/gvltest.c
+++ ext/gvltest/gvltest.c
@@ -6,20 +6,38 @@
 
 static VALUE rb_mGVLTest;
 
-static VALUE sleep_holding_gvl(VALUE self, VALUE timev) {
-    double time = NUM2DBL(timev);
-    unsigned long long int ns = time * 1E9;
-    struct timespec ts = { ns / 1000000000LL, ns % 1000000000LL };
-
+static void durable_sleep(struct timespec ts) {
     int res;
     do {
         res = nanosleep(&ts, &ts);
     } while (res && errno == EINTR);
+}
 
+static VALUE sleep_holding_gvl(VALUE self, VALUE timev) {
+    double time = NUM2DBL(timev);
+    unsigned long long int ns = time * 1E9;
+    struct timespec ts = { ns / 1000000000LL, ns % 1000000000LL };
+    durable_sleep(ts);
     return timev;
 }
 
-static void *nop(void *) {
+static void *sleep_without_gvl_i(void *tsv) {
+    struct timespec *ts = tsv;
+    durable_sleep(*ts);
+    return NULL;
+}
+
+static VALUE sleep_without_gvl(VALUE self, VALUE timev) {
+    double time = NUM2DBL(timev);
+    unsigned long long int ns = time * 1E9;
+    struct timespec ts = { ns / 1000000000LL, ns % 1000000000LL };
+
+    rb_thread_call_without_gvl(sleep_without_gvl_i, &ts, NULL, NULL);
+    return Qnil;
+}
+
+
+static void *nop(void *unused) {
     return NULL;
 }
 
@@ -33,5 +51,6 @@ Init_gvltest(void)
 {
   rb_mGVLTest = rb_define_module("GVLTest");
   rb_define_singleton_method(rb_mGVLTest, "sleep_holding_gvl", sleep_holding_gvl, 1);
+  rb_define_singleton_method(rb_mGVLTest, "sleep_without_gvl", sleep_without_gvl, 1);
   rb_define_singleton_method(rb_mGVLTest, "nogvl_nop", nogvl_nop, 0);
 }
